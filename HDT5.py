62bd15978a0f13c5ee0559518e45cd7c1f4eda6c
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-03-09 17:49:21 -0600

    Creacion de proceso

--- HDT5.py
+++ HDT5.py
@@ -2,10 +2,11 @@ import simpy
 import random
 
 def proceso (env,CPU, RAM):
+    #env, name, bcs, driving_time, charge_duration
     asignacion_memoria = random.randit(1,10)
     RAM.get(asignacion_memoria)
     #Tiempo de Trabajo de la CPU
-    tiempoTrabajo = random.randit(1,7)
+    tiempoInicial = env.now
 
     if RAM != 0:
         print("READY")
@@ -17,11 +18,21 @@ def proceso (env,CPU, RAM):
         yield turno #Entra el proceso a la CPU
         yield env.timeout(tiempoTrabajo) #
 
+    tiempoTrabajo = env.now - tiempoInicial
+
+    global totalProceso
+    totalProceso += totalProceso + tiempoTrabajo
+
     RAM.put(asignacion_memoria)
     
 #---------------------
+totalProceso = 0
 RAM = simpy.Container(env, init=100, capacity=100)
 env = simpy.Enviroment() #Ambiente de simulación
 CPU = simpy.Resource(env,capacity = 3)
 
-proceso(CPU, env, RAM)
+for i in range(25):
+    env.proces(proceso(env, CPU, RAM, random.expovariate(1.0/10), 1))
+
+#inicia la simulacion
+env.run()
