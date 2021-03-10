8d76925f858876768b6ea0e083de4ff24e7432c3
TheJPMZ <64183934+TheJPMZ@users.noreply.github.com>
2021-03-10 17:52:14 -0600

    Debugging

--- HDT5.py
+++ HDT5.py
@@ -1,68 +1,72 @@
 import simpy
 import random
 
-def proceso (env,CPU, RAM, procesos, speed, name):
+
+def proceso(env, CPU, RAM, tiempoEntrada, speed, name):
+    yield env.timeout(tiempoEntrada)
     # env, name, bcs, driving_time, charge_duration
-    asignacion_memoria = random.randint(1,10)
+    asignacion_memoria = random.randint(1, 10)
     # Tiempo de Trabajo de la CPU
     tiempoInicial = env.now
 
     # NEW
     while True:
         if RAM.level >= asignacion_memoria:
-            RAM.get(asignacion_memoria)
-            print(str(name)+" usa "+str(asignacion_memoria))
-            print(str(RAM.level)+" en el ram")
+            yield RAM.get(asignacion_memoria)
+            print('\nEl nuevo proceso %s usa %s de la memoria RAM' % (name, asignacion_memoria))
             break;
-        #else:
-            #print("Esperando")
+        # else:
+        # print("Esperando")
 
     # Instrucciones a realizar
     instrucciones = random.randint(1, 10)
     waiting = 0
+    exit = False
 
-    exit = false;
     # COLA DE READY
-    while exit == false:
-        print("Cantidad de instrucciones de "+str(name)+": "+str(instrucciones))
+    while exit == False:
         # RUNNING
-        tiempoTrabajo = env.now - tiempoInicial
-
         # Si hay otros 3 procesos, deberá hacer cola
-        with CPU.request() as turno:
-            yield turno  # Entra el proceso a la CPU
-            yield env.timeout(tiempoTrabajo)  # El proceso se trabaja en la CPU por un tiempo
+        with CPU.request() as req:
+            yield req  # Entra el proceso a la CPU
+            yield env.timeout(speed)  # El proceso se trabaja en la CPU por un tiempo
 
-        if instrucciones < 3: # Si hay menos de tres instrucciones, la resta da 0
-            instrucciones = 0 # TERMINATED
+        if instrucciones < 3:  # Si hay menos de tres instrucciones, la resta da 0
 
-            exit = true
+            instrucciones = 0  # TERMINATED
+            print('El proceso %s se encuentra en estado TERMINATED' % (name))
+            exit = True
         elif instrucciones >= 3:
             instrucciones -= 3
             waiting = random.randint(1, 2)  # Se genera numero al azar, 1 sale y 2 sigue
             if waiting == 1:
                 #  entra a WAITING
-                print("WAITING")
+                print('El proceso %s se encuentra en estado WAITING' % (name))
 
-
-
+    tiempoTrabajo = env.now - tiempoInicial
 
     global totalProceso
     totalProceso += totalProceso + tiempoTrabajo
-    print("Se asigna: "+str(asignacion_memoria))
+    global procesos
+    procesos += 1
+    print("Se devuelve " + str(asignacion_memoria) + " a la memoria RAM")
     RAM.put(asignacion_memoria)
-    print(RAM.level)
-    
+    print('La RAM tiene actualmente %s de memoria disponible' % (RAM.level))
+
+
 # ---------------------
 totalProceso = 0
+procesos = 0
 
 random.seed(69)
-env = simpy.Environment() # Ambiente de simulación
+env = simpy.Environment()  # Ambiente de simulación
 RAM = simpy.Container(env, init=100, capacity=100)
-CPU = simpy.Resource(env,capacity = 1)
+CPU = simpy.Resource(env, capacity=3)
 
-for i in range(25):
-    env.process(proceso(env, CPU, RAM, random.expovariate(1.0/10), 1, i+1))
+for i in range(5):
+    env.process(proceso(env, CPU, RAM, random.expovariate(1.0 / 10), 1, i + 1))
 
 # inicia la simulacion
 env.run()
+print("\n\tEl tiempo total es de : " + str(totalProceso))
+print("\tSe realizaron " + str(procesos) + " procesos.")
