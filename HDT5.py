380ef41219e147913a75efa796dd9939c6012843
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-03-10 22:38:17 -0600

    Correccion de variables

--- HDT5.py
+++ HDT5.py
@@ -7,14 +7,14 @@ def proceso(env, CPU, RAM, tiempoEntrada, speed, name):
     # env, name, bcs, driving_time, charge_duration
     asignacion_memoria = random.randint(1, 10)
     # Tiempo de Trabajo de la CPU
-    tiempoInicial = env.now
+    tiempoInicial = float(env.now)
 
     # NEW
-    while True:
-        if RAM.level >= asignacion_memoria:
-            yield RAM.get(asignacion_memoria)
-            print('\nEl nuevo proceso %s usa %s de la memoria RAM' % (name, asignacion_memoria))
-            break
+    with RAM.get(asignacion_memoria) as getRAM:
+        yield getRAM
+        print('\nEl nuevo proceso %s usa %s de la memoria RAM' % (name, asignacion_memoria))
+
+    global capacidad_instrucciones
 
     # Instrucciones a realizar
     instrucciones = random.randint(1, 10)
@@ -29,24 +29,31 @@ def proceso(env, CPU, RAM, tiempoEntrada, speed, name):
             yield req  # Entra el proceso a la CPU
             yield env.timeout(speed)  # El proceso se trabaja en la CPU por un tiempo
 
-        if instrucciones < 3:  # Si hay menos de tres instrucciones, la resta da 0
+        if instrucciones < capacidad_instrucciones:  # Si hay menos de tres instrucciones, la resta da 0
 
             instrucciones = 0  # TERMINATED
             print('El proceso %s se encuentra en estado TERMINATED' % (name))
             exit = True
-        elif instrucciones >= 3:
-            instrucciones -= 3
+
+        elif instrucciones >= capacidad_instrucciones:
+            instrucciones -= capacidad_instrucciones
             waiting = random.randint(1, 2)  # Se genera numero al azar, 1 sale y 2 sigue
+
             if waiting == 1:
                 #  entra a WAITING
                 print('El proceso %s se encuentra en estado WAITING' % (name))
+                yield env.timeout(2)
+                print('El proceso %s ya no se encuentra en estado WAITING' % (name))
 
-    tiempoTrabajo = env.now - tiempoInicial
+
+    tiempoTrabajo = float(env.now) - tiempoInicial
 
     global totalProceso
-    totalProceso += totalProceso + tiempoTrabajo
-    global procesos
-    procesos += 1
+    totalProceso += tiempoTrabajo
+
+    global numeros_procesos
+    numeros_procesos += 1
+
     print("Se devuelve " + str(asignacion_memoria) + " a la memoria RAM")
     RAM.put(asignacion_memoria)
     print('La RAM tiene actualmente %s de memoria disponible' % (RAM.level))
@@ -54,17 +61,22 @@ def proceso(env, CPU, RAM, tiempoEntrada, speed, name):
 
 # ---------------------
 totalProceso = 0
-procesos = 0
+numeros_procesos = 0
+capacidad = 1
+capacidad_instrucciones = 3
+intervalos = 10
+capacidad_procesos = 200
 
 random.seed(10)
+
 env = simpy.Environment()  # Ambiente de simulación
 RAM = simpy.Container(env, init=100, capacity=100)
-CPU = simpy.Resource(env, capacity=1)
+CPU = simpy.Resource(env, capacity=capacidad)
 
-for i in range(25):
-    env.process(proceso(env, CPU, RAM, random.expovariate(1.0 / 10), 1, i + 1))
+for i in range(capacidad_procesos):
+    env.process(proceso(env, CPU, RAM, random.expovariate(1.0 / intervalos), 1, i + 1))
 
 # inicia la simulacion
 env.run()
-print("\n\tEl tiempo total es de : " + str(totalProceso/25))
-print("\tSe realizaron " + str(procesos) + " procesos.")
+print("\n\tEl promedio de tiempo por proceso es de : " + str(totalProceso/capacidad_procesos))
+print("\tSe realizaron " + str(numeros_procesos) + " procesos.")
